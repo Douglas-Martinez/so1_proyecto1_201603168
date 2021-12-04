a4bc50540c06f4672cd6ad0fa5e9cf9135413c29
Douglas Martinez <douglasmartines.ecys@gmai.com>
2021-12-04 04:53:35 -0600

    Creacion modulo de RAM

---
+++ ram_module/memo_201603168.c
@@ -0,0 +1,73 @@
+#include <linux/sysinfo.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/fs.h>
+#include <linux/mm.h>
+#include <linux/seq_file.h>
+#include <linux/proc_fs.h>
+#include <linux/init.h>
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("DOUGLAS MARTINEZ");
+MODULE_DESCRIPTION("RAM INFO MODULE");
+MODULE_VERSION("1.0.0");
+
+struct sysinfo si;
+
+static int show_ram_data(struct seq_file *m, void *v)
+{
+    #define K(x) ((x) << (PAGE_SHIFT - 10))
+
+    si_meminfo(&si);
+    int tot = (K(si.totalram)/1024) & __INT_MAX__;
+    int tmp = (K(si.totalram - si.freeram - si.bufferram - si.sharedram)/1024) & __INT_MAX__;
+    int con = tot - tmp;
+
+    seq_printf(m, "TOTAL RAM: %8d MB\nCONSUMIDA: %8d MB\nPct Consumo: %6d %%\n", tot, con, (con*100)/tot);
+
+    return 0;
+}
+
+static ssize_t write_file_proc(struct file *file, const char __user *buffer, size_t count, loff_t *f_pos)
+{
+    return 0;
+}
+
+static int open_file_proc(struct inode *inode, struct file *file)
+{
+    return single_open(file, show_ram_data, NULL);
+}
+
+static struct file_operations fops = {
+    .owner = THIS_MODULE,
+    .open = open_file_proc,
+    .release = single_release,
+    .read = seq_read,
+    .llseek = seq_lseek,
+    .write = write_file_proc
+};
+
+static int __init ram_read_init(void)
+{
+    struct proc_dir_entry *entry;
+    entry = proc_create("memo_201603168", 0777, NULL, &fops);
+
+    if(!entry) 
+    {
+        return -1;
+    } else 
+    {
+        printk(KERN_INFO "Start RAM - 'cat /proc/memo_201603168'\n");
+    }
+
+    return 0;
+}
+
+static void __exit ram_read_exit(void)
+{
+    remove_proc_entry("memo_201603168", NULL);
+    printk(KERN_INFO "End RAM\n");
+}
+
+module_init(ram_read_init)
+module_exit(ram_read_exit)
