19a058568b32995633f4e729850dd63cd550dcef
Douglas Martinez <douglasmartines.ecys@gmai.com>
2021-12-15 02:44:48 -0600

    Algunos cambios visuales a Procesos

--- web_server/src/components/Procesos.js
+++ web_server/src/components/Procesos.js
@@ -52,9 +52,9 @@ const Procesos = () => {
     } else {
         return (
             <React.Fragment>
-                <div className="d-grid col-12 col-lg-10 mx-auto">
+                <div className="d-grid col-12 col-lg-10 mx-auto text-center align-middle">
                     <br />
-                    <h1 className="text-center">Procesos</h1>
+                    <h1>Procesos</h1>
                     <br />
                     <div className="d-grid col-12 col-lg-5 mx-auto">
                         <Table striped bordered hover variant="dark">
@@ -65,23 +65,23 @@ const Procesos = () => {
                                 </tr>
                             </thead>
                             <tbody>
-                                <tr key="1" className="align-middle">
-                                    <td>Ejecucion (Running)</td>
+                                <tr key="1">
+                                    <td className="text-left">Ejecucion (Running)</td>
                                     <td>{procesos.EJECUCION}</td>
                                 </tr>
-                                <tr key="2" className="align-middle">
+                                <tr key="2">
                                     <td>Suspendidos (Sleeping)</td>
                                     <td>{procesos.SUSPENDIDOS}</td>
                                 </tr>
-                                <tr key="3" className="align-middle">
+                                <tr key="3">
                                     <td>Detenidos (Stopped)</td>
                                     <td>{procesos.DETENIDOS}</td>
                                 </tr>
-                                <tr key="4" className="align-middle">
+                                <tr key="4">
                                     <td>Procesos Zombie</td>
                                     <td>{procesos.ZOMBIE}</td>
                                 </tr>
-                                <tr key="5" className="align-middle">
+                                <tr key="5">
                                     <td>Total de Procesos</td>
                                     <td>{procesos.TOTAL}</td>
                                 </tr>
@@ -109,34 +109,23 @@ const Procesos = () => {
                                 {
                                     procesos && procesos.PROCESOS.map((proc) => {
                                         return (
-                                            <tr key={proc.PID} className="align-middle" data-toggle="collapse" data-target="#LOL1">
+                                            <tr key={proc.PID}>
                                                 <td>
                                                     <button 
                                                         className="btn btn-//#endregion"
-                                                        type="button"
                                                         data-toggle="collapse"
-                                                        data-target=""
-                                                        aria-expanded="false"
-                                                        aria-controls="">{'->'}</button>
+                                                        data-target={`#collapse${+ proc.PID}`}
+                                                        aria-expanded="true"
+                                                        aria-controls={`collapse${+ proc.PID}`}>{'->'}</button>
                                                 </td>
                                                 <td>{proc.PID}</td>
                                                 <td>{proc.NOMBRE}</td>
                                                 <td>{proc.UNAME}</td>
                                                 <td>{proc.ENAME}</td>
-                                                <td className="text-right" >{proc.RAM}</td>
+                                                <td>{proc.RAM}</td>
                                                 <td>{proc.RAM_BYTES}</td>
                                                 <td>
                                                     <button className="btn btn-danger m-2" onClick={() => killProceso(proc.PID)}>Kill</button>
-                                                </td>
-                                                <td colSpan="4">
-                                                    <Table size="sm">
-                                                        <tbody>
-                                                            <tr>
-                                                                <th>ALV</th>
-                                                                <td>FFF</td>
-                                                            </tr>
-                                                        </tbody>
-                                                    </Table>
                                                 </td>
                                             </tr>
                                         )
